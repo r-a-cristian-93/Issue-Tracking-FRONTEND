3e385bcd717f22281aff8ac6294b0b06df740310
Alex <r.a.cristian.93@gmail.com>
2020-12-15 20:10:24 +0200

    Implemented ticket filter endpoint.  * Temporary solution

--- helpdesk-rest-maven/src/main/java/rest/tickets/TicketsController.java
+++ helpdesk-rest-maven/src/main/java/rest/tickets/TicketsController.java
@@ -3,22 +3,40 @@ package rest.tickets;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.data.domain.Example;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.*;
 
 @Controller
 @RequestMapping("tickets")
 public class TicketsController {
-	@Autowired private TicketsDetailsRepository ticketsDetailsRepo;
+	@PersistenceContext private EntityManager entityManager;
 		
 	@ResponseBody
-	@GetMapping("/get/ex")
-	public Iterable<TicketDetailsModel> getByEx(
-		@RequestParam("status") String status
-	){
-		System.out.println(status);
-		TicketDetailsModel ex = TicketDetailsModel.builder().status(status).build();
+	@GetMapping("/get/filter")
+	public List<TicketDetailsModel> getByEx(
+		@RequestParam(value = "id", required = false) Integer id,
+		@RequestParam(value = "openedByDepartment", required = false) String openedByDepartment,
+		@RequestParam(value = "openedBy", required = false) Integer openedBy,
+		@RequestParam(value = "assignedTo", required = false) Integer assignedTo,
+		@RequestParam(value = "concernedDepartment", required = false) String concernedDepartment,
+		@RequestParam(value = "status", required = false) String status ){	
+				
+		TicketFilterBean filter = new TicketFilterBean()
+			.setTicketId(id)
+			.setOpenedBy(openedBy)
+			.setAssignedTo(assignedTo)
+			.setStatus(status)
+			.setOpenedByDepartment(openedByDepartment)
+			.setConcernedDepartment(concernedDepartment);
 		
-		System.out.println(ex.getStatus());
-		return ticketsDetailsRepo.findAll(Example.of(ex));
+		String sql = "SELECT t.ID, t.status, t.summary, t.issue, uo.email opened_by, uo.department opened_by_department, ua.email assigned_to, uc.email closed_by "+ 
+					"FROM tickets t "+
+					"LEFT JOIN users uo ON t.opened_by=uo.ID "+
+					"LEFT JOIN users ua ON t.assigned_to=ua.ID "+
+					"LEFT JOIN users uc ON t.closed_by=uc.ID " +
+					 filter + ";";
+		List results = entityManager.createNativeQuery(sql, TicketDetailsModel.class).getResultList();
+		return results;
 	}
 }
--- helpdesk-rest-maven/src/main/java/rest/tickets/TicketsDetailsRepository.java
+++ helpdesk-rest-maven/src/main/java/rest/tickets/TicketsDetailsRepository.java
@@ -6,15 +6,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.domain.Example;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.data.repository.query.Param;
 
 
 public interface TicketsDetailsRepository extends JpaRepository<TicketDetailsModel, Integer> {
-	@Query(value="SELECT t.ID, t.status, t.summary, t.issue, uo.email opened_by, uo.department opened_by_department, ua.email assigned_to, uc.email closed_by "+ 
-					"FROM tickets t "+
-					"LEFT JOIN users uo ON t.opened_by=uo.ID "+
-					"LEFT JOIN users ua ON t.assigned_to=ua.ID "+
-					"LEFT JOIN users uc ON t.closed_by=uc.ID ",
-			nativeQuery=true)
-	@Override	
-	<S extends TicketDetailsModel> List<S> findAll(Example<S> example);
+	
 }
